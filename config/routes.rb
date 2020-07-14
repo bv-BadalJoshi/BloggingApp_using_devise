5e06e57c67fa1dc4c832138f161f8f382e92a8a0
bv-BadalJoshi <Badal@blogvault.net>
2020-07-14 12:18:08 +0530

    initial

--- config/routes.rb
+++
@@ -1,8 +0,0 @@
-Rails.application.routes.draw do
-	devise_for :admin_users, ActiveAdmin::Devise.config
-	ActiveAdmin.routes(self)
-	devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
-	resources :articles
-	root "pages#home"
-	resources :pages, only: [:show]	
-end
